e1a85413c78eeea02e15abfecba26e2c7d84cf71
ZH_Tailscale <bioinfo.gao@gmail.com>
2026-06-03 23:39:07 -0400

    main

---
+++ Lynnda_Exercise_Files/04 Syntax/0401_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# syntax.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+def main():
+    print("This is the syntax.py file.")
+    egg()
+
+def egg():
+    print("This is the egg function.")
+
+if __name__ == "__main__": 
+    main()
