1aa3e859c67a14d1b03abac20cdeb5e0a3ffe3a9
ZH_Tailscale <bioinfo.gao@gmail.com>
2026-06-05 19:47:23 -0400

    7

--- Lynnda_Exercise_Files/07 Loops/iterators.py
+++ Lynnda_Exercise_Files/07 Loops/iterators.py
@@ -3,6 +3,7 @@
 # This is an exercise file from Python 3 Essential Training on lynda.com
 # Copyright 2010 The BearHeart Group, LLC
 
+# any container object is iterable
 def main():
     #fh = open('lines.txt')
     fh = open('/home/gao/Python_Tools_Tested/Lynnda_Exercise_Files/07 Loops/lines.txt')
